8990faa0707c00b00c19763e51bc5b0291cfe59c
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-02-04 10:48:32 -0300

    adicionei mais forms

--- praticasv2/script.js
+++ praticasv2/script.js
@@ -64,3 +64,66 @@ if(hora < 12){
     }
 
     date.innerHTML += ` de ${ano}`
+
+function calcimc(){
+    var peso = document.getElementById('peso')
+    var p = Number(peso.value)
+    var altura = document.getElementById('altura')
+    var a = Number(altura.value)
+
+    var imc = p / (a**2)
+
+    var resimc = document.getElementById('resimc')
+
+    resimc.innerHTML = `Seu IMC é ${imc.toFixed(2)}!`
+    console.log(imc)
+
+    if (imc < 18.5){
+        resimc.innerHTML += `\nVocê está abaixo do peso.`
+    } else if ((imc > 18.6) && (imc < 24.9)){
+        resimc.innerHTML += `\nVocê está no peso ideal, parabéns.`
+    } else if ((imc > 25) && (imc < 29.9)){
+        resimc.innerHTML += `\nVocê está levemente acima do peso.`
+    } else if ((imc > 30) && (imc < 34.9)){
+        resimc.innerHTML += `\nVocê está com obesidade grau 1.`
+    } else if ((imc > 35) && (imc < 39.9)){
+        resimc.innerHTML += `\nVocê está com obesidade grau 2(severa).`
+    } else {
+        resimc.innerHTML += `\nVocê está com obesidade grau 3(mórbida)`
+    }
+}
+
+function porcentagem(){
+    var porc = document.getElementById('porc')
+    var p = Number(porc.value)
+
+    var valor = document.getElementById('valor')
+    var v = Number(valor.value)
+
+    var resporc = document.getElementById('resporc')
+
+    var res = (p/100) * v
+
+    resporc.innerHTML = res
+}
+
+
+function tabuada(){
+    let num = document.getElementById('num')
+    let tab = document.getElementById('tab')
+
+    if(num.value.length == 0){
+        alert('Digite um número')
+    } else if (num.value.length >= 10){
+        alert('Máximo de caracteres excedido (no máximo 10)')
+    } else {
+        tab.innerHTML = ''
+        let n = Number(num.value)
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
